e45737430b1d08a51bbfee351553a41d6df86319
Tevin Mantock <tmantock@gmail.com>
2017-02-06 18:54:21 -0800

    writing http request

---
+++ servers/read/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+	defer conn.Close()
+	//Never get here
+	//Open stream
+	//Never get here because scanning is an infinite loop when listening for a request over tcp
+	fmt.Println("Code got here.")
+}
