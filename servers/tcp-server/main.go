27ee197a6e4f629646b670ed8e2518a7ec01ad94
Tevin Mantock <tmantock@gmail.com>
2017-02-11 17:10:22 -0800

    TCP server

---
+++ servers/tcp-server/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	//read request
+	request(conn)
+
+	//write response
+	respond(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			//request line
+			m := strings.Fields(ln)[0]
+			fmt.Println("***METHOD", m)
+		}
+		if ln == "" {
+			//headers are done
+			break
+		}
+		i++
+	}
+}
+
+func respond(conn net.Conn) {
+	body := `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>GO TCP Server</title>
+            </head>
+            <body>
+                <h1>Hello Mars!</h1>
+            </body>
+        </html>
+    `
+	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprintf(conn, "Content-Type: text/html\r\n")
+	fmt.Fprintf(conn, "\r\n")
+	fmt.Fprintf(conn, body)
+}
