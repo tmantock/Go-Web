a56372e5415171df3a0240e4c43611fec432186e
Tevin Mantock <tmantock@gmail.com>
2017-02-14 14:55:02 -0800

    more primes and factors

--- math/prime/main.go
+++ math/prime/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	p := primesList(100)
-	fmt.Println(p)
+	flag.Parse()
+	fn := strings.ToUpper(flag.Arg(0))
+	nm, _ := strconv.Atoi(flag.Arg(1))
+
+	switch fn {
+	case "LIST":
+		result := primesList(nm)
+		fmt.Println(result)
+	case "PRIME":
+		result := isPrime(nm)
+		fmt.Println(result)
+	case "FACTORS":
+		result := primeFactors(nm)
+		fmt.Println(result)
+	default:
+		fmt.Println("Invalid Command")
+	}
+
+	os.Exit(0)
+}
+
+func isPrime(num int) bool {
+	if num < 2 {
+		return false
+	}
+
+	sqrt := int(math.Floor(math.Sqrt(float64(num))))
+
+	for i := 2; i <= sqrt; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
 
 func primesList(limit int) []int {
@@ -33,4 +70,18 @@ func primesList(limit int) []int {
 	}
 
 	return primes
+}
+
+func primeFactors(num int) []int {
+	var factors []int
+	isPrime := num
+
+	for i := 2; i <= isPrime; i++ {
+		if isPrime%i == 0 {
+			isPrime = isPrime / i
+			factors = append(factors, i)
+		}
+	}
+
+	return factors
 }
