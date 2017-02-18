4dfe47fc25869cbdd0ca75631d82e545858fec25
Tevin Mantock <tmantock@gmail.com>
2017-02-17 18:01:02 -0800

    exploring the request

---
+++ net-http/method/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method        string
+		URL           *url.URL
+		Submissions   map[string][]string
+		Header        http.Header
+		Host          string
+		ContentLength int64
+	}{
+		r.Method,
+		r.URL,
+		r.Form,
+		r.Header,
+		r.Host,
+		r.ContentLength,
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
