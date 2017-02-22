e483b80f0190496c3c715637d41c7430acb17339
Tevin Mantock <tmantock@gmail.com>
2017-02-21 18:58:54 -0800

    serve content and serve files

---
+++ files/serveFile/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", dog)
+	http.HandleFunc("/toby.jpg", dogPic)
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="toby.jpg">`)
+}
+
+func dogPic(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("toby.jpg")
+	if err != nil {
+		http.Error(w, "File Not Found", 404)
+		return
+	}
+	defer f.Close()
+
+	// fi, err := f.Stat()
+	// if err != nil {
+	// 	http.Error(w, "File Not Found", 404)
+	// 	return
+	// }
+
+	//http.ServeContent(w, r, f.Name(), fi.ModTime(), f)
+
+	//io.Copy(w, f)
+
+	http.ServeFile(w, r, "toby.jpg")
+}
