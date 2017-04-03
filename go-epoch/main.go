09ee3cd57fb1eb0dbb22052f93beebe940a82f54
Tevin Mantock <tmantock@gmail.com>
2017-04-03 09:55:05 -0700

    error handling

--- go-epoch/main.go
+++ go-epoch/main.go
@@ -11,33 +11,23 @@ import (
 func main() {
 	var cmd string
 
-	cmd = strings.ToUpper(os.Args[1])
+	if len(os.Args) > 0 {
 
-	if cmd == "TIME" {
-		stamp, err := strconv.ParseInt(os.Args[2], 10, 64)
-		if err != nil {
-			panic(err)
+		cmd = strings.ToUpper(os.Args[1])
+
+		if cmd == "TIME" {
+			var tm time.Time
+
+			if len(os.Args) > 2 {
+				stamp, err := strconv.ParseInt(os.Args[2], 10, 64)
+				if err != nil {
+					panic(err)
+				}
+				tm = time.Unix(stamp, 0)
+			} else {
+				tm = time.Now()
+			}
+			fmt.Println(tm)
 		}
-		tm := time.Unix(stamp, 0)
-		fmt.Println(tm)
 	}
-
-	// if cmd == "EPOCH" {
-	// 	var str string
-
-	// 	strtime := os.Args[2:]
-
-	// 	for _, v := range strtime {
-	// 		str = str + " " + v
-	// 	}
-
-	// 	t, err := time.Parse(nil, str)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	// 	fmt.Println(t.Unix())
-
-	// }
-
 }
